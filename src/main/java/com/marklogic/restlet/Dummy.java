9aff312700d38b9f8d57a7da9d60cfc22b7b9288
Justin Makeig <jmpublic@makeig.com>
2014-01-10 14:44:08 -0800

    Initial commit

---
+++ src/main/java/com/marklogic/restlet/Dummy.java
@@ -0,0 +1,20 @@
+package com.marklogic.restlet;
+
+import org.restlet.Server;
+import org.restlet.data.Protocol;
+import org.restlet.resource.Get;
+import org.restlet.resource.ServerResource;
+
+public class Dummy extends ServerResource {
+
+	public static void main(String[] args) throws Exception {
+		// Create the HTTP server and listen on port 8182
+		new Server(Protocol.HTTP, 8182, Dummy.class).start();
+	}
+
+	@Get
+	public String toString() {
+		return "hello, world";
+	}
+
+}
